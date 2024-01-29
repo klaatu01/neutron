ea0ab8c5eaccd6ee453cdef9f6ef3f6b2427ee8e
Charles Ede <charlieede01@gmail.com>
2024-01-29 23:09:40 +0000

    fix examples

--- src/consumer.rs
+++ src/consumer.rs
@@ -1,3 +1,5 @@
+use std::fmt::{Display, Formatter};
+
 use crate::message::ClientInbound;
 use async_trait::async_trait;
 
@@ -6,9 +8,23 @@ use serde::de::DeserializeOwned;
 
 use crate::message::proto::pulsar::MessageIdData;
 
+#[derive(Debug)]
 pub enum PulsarConsumerError {
     PulsarError(String),
     DeserializationError(String),
+}
+
+impl std::error::Error for PulsarConsumerError {}
+
+impl Display for PulsarConsumerError {
+    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+        match self {
+            PulsarConsumerError::PulsarError(msg) => write!(f, "PulsarError: {}", msg),
+            PulsarConsumerError::DeserializationError(msg) => {
+                write!(f, "DeserializationError: {}", msg)
+            }
+        }
+    }
 }
 
 #[async_trait]
@@ -40,6 +56,20 @@ pub struct ConsumerConfig {
 pub struct Consumer {
     client: crate::Pulsar,
     config: ConsumerConfig,
+}
+
+impl Consumer {
+    pub fn new(client: crate::Pulsar, config: ConsumerConfig) -> Self {
+        Consumer { client, config }
+    }
+
+    pub async fn connect(mut self) -> Result<Self, PulsarConsumerError> {
+        self.client
+            .connect()
+            .await
+            .map_err(|_| PulsarConsumerError::PulsarError("Failed to connect".to_string()))?;
+        Ok(self)
+    }
 }
 
 pub struct Message<T> {
